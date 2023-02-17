492b13b9949f86818856a70b56ca46ca409e67b8
Frerich Raabe <frerich.raabe@gmail.com>
2023-02-17 13:33:23 +0100

    Made everything a plain function component

--- lib/trix_editor.ex
+++ lib/trix_editor.ex
@@ -1,12 +1,25 @@
 defmodule TrixEditor do
   use Phoenix.Component
 
-  import Phoenix.Component, only: [live_component: 1]
+  import Phoenix.HTML.Form, only: [hidden_input: 3]
+
+  css_path = "assets/node_modules/trix/dist/trix.css"
+
+  @external_resource css_path
+  @trix_css File.read!(css_path)
 
   def trix_editor(assigns) do
-    assigns
-    |> Map.put_new(:id, :trix_editor)
-    |> Map.put(:module, __MODULE__.Component)
-    |> live_component()
+    assigns =
+      assigns
+      |> assign_new(:id, :trix_editor)
+      |> assign(:trix_css, @trix_css)
+
+    ~H"""
+    <div id={@id} phx-update="ignore">
+      <style><%= {:safe, @trix_css} %></style>
+      <%= hidden_input(@for, @field, id: "#{@id}_input") %>
+      <trix-editor input={"#{@id}_input"}></trix-editor>
+    </div>
+    """
   end
 end
