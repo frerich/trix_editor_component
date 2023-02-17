21f290efe9514d3dede44dd8e7aa9959254d903d
Frerich Raabe <frerich.raabe@gmail.com>
2023-02-17 16:21:00 +0100

    Claim support Elixir 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TrixEditor.MixProject do
     [
       app: :trix_editor_component,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
